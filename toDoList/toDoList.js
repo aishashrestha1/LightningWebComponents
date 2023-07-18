5d9c29d57b01903ea8e3b85937be9c1ed87b68c6
aishashrestha1 <aishashrestha33@gmail.com>
2023-07-18 15:55:40 -0500

    To do list working

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -1,7 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+
 
 export default class ToDoList extends LightningElement {
 
+   TodoId = 2;
+   @track todos = [
+    { id: 1, taskDetails: 'Write Story' },
+    { id: 2, taskDetails: 'Build Framework' }
+];
+
+    @track taskDetails;
+
+    handlechange(event){
+        this.taskDetails = event.target.value;
+        //console.log('New Task:' + this.taskDetails);
+    }
+
+    addClick() {
+
+            this.TodoId = this.TodoId + 1;
+              this.todos = [
+                  ...this.todos,
+                  {
+                      id: this.TodoId,
+                      taskDetails: this.taskDetails,
+                  }
+              ];
+          }
+        
+       
 
 
 
